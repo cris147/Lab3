b1d04485062067f0136a499848544a6dc15638e2
Cris147 <cristopher20061312@gmail.com>
2024-10-25 15:26:22 -0600

    Segundo commit

--- src/lab3p1_cristopher_sibrian/Lab3P1_Cristopher_Sibrian.java
+++ src/lab3p1_cristopher_sibrian/Lab3P1_Cristopher_Sibrian.java
@@ -19,7 +19,7 @@ public class Lab3P1_Cristopher_Sibrian {
         // TODO code application logic here
         Scanner lab = new Scanner(System.in);
         char resp;
-        int menu, r, n, CR;
+        int menu, r, n, game = 1;
         do{
             System.out.println("Bienvenido al juego del lab3");
             System.out.println("Que juego desea jugar?");
@@ -33,13 +33,49 @@ public class Lab3P1_Cristopher_Sibrian {
                     System.out.println("Ingrese el tamaño que desea el reloj");
                     break;
                 case 2: 
-                    System.out.println("Ingrese el valor de r: ");
-                    r = lab.nextInt();
                     System.out.println("Ingrese el valor de n: ");
                     n = lab.nextInt();
-                    
-                    CR = (r / ((n-r) * r) );
-                    System.out.println(CR);
+                    System.out.println("Ingrese el valor de r: ");
+                    r = lab.nextInt();
+                    if (r<= n){
+                        long nfac = 1;    
+                        for (int o = 1; o<= n; o++){
+                            nfac *= o;}
+
+                        long nrfac = 1;
+                        for (int o = 1; o<= (n-r); o++){
+                            nrfac *= o;}
+
+                        long rfac = 1;
+                        for (int o = 1; o<= r; o++){
+                            rfac *= o;}
+
+                        long CR = (int) nfac/(nrfac * rfac);
+                        System.out.println("El total de combinaciones es " + CR);
+                    }else{
+                        System.out.println("Los numeros de R no puesen ser mayores a los de N.");
+                    }
+                    break;
+                case 3:
+                    do{
+                        System.out.println("Ingrese el tamano del sandwich");
+                        game = lab.nextInt();
+                        if (game >= 7 && game%2!=0){
+                            for (int y = 1; y<= game; y++){
+                                for (int w = 1; w<= game; w++ ){
+                                    if (y== w){
+                                        System.out.print(" ");
+                                        continue;
+                                    }
+                                    System.out.print("* ");
+                                }
+                                System.out.println("");
+
+                            }
+                        }else {
+                            System.out.println("El numero debe ser mayor a 7 y tambien debe ser impar");
+                        }
+                    }while (game < 7 || game%2==0);
                     break;
             }
             System.out.println("Desea jugar otro juego? ");
